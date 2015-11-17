01eeec827bd98387444454d776ae551d28fd0585
administrator@Waltahr <jpuckett@walton.uark.edu>
2015-11-17 15:52:27 -0600

    add gedit-plugins

--- apt-get-installer.bash
+++ apt-get-installer.bash
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# latest version of git
 sudo add-apt-repository ppa:git-core/ppa
 sudo apt-get update
 sudo apt-get install git -y
+
+# plugins for gedit
+if $(command -v gedit >/dev/null 2>&1); then
+	sudo apt-get install gedit-plugins
+fi
