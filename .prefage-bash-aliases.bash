36736bd4c259f55fcad6b0bf03b7001924330d2d
admonkey <jeffpuckett2@gmail.com>
2015-11-16 20:40:55 -0600

    add crontaball alias to show every crontab

--- .prefage-bash-aliases.bash
+++ .prefage-bash-aliases.bash
@@ -3,3 +3,4 @@ alias c='clear'
 alias g='git'
 alias alog='sudo cat /var/log/apache2/access.log'
 alias elog='sudo cat /var/log/apache2/error.log'
+alias crontaball="cat /etc/passwd | sed 's/^\([^:]*\):.*$/sudo crontab -u \1 -l 2>\&1/' | sh | grep -v 'no crontab for'"
