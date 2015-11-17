5b13e3d4d4f1d3c54555e4d3f19a720a02a30199
administrator@Waltahr <jpuckett@walton.uark.edu>
2015-11-17 13:31:47 -0600

    rearchitect installation by including original files from anywhere to ease backporting

---
+++ bash/bashrc-include.bash
@@ -0,0 +1,21 @@
+# move to working directory
+cd $( dirname "${BASH_SOURCE[0]}" )
+cd ..
+export prefage_directory=$(pwd)
+
+. $prefage_directory/bash/ubuntu14.04-bashrc.bash
+. $prefage_directory/bash/aliases.bash
+. $prefage_directory/bash/colors.bash
+
+if $(command -v git >/dev/null 2>&1); then
+   . $prefage_directory/git/completion.bash
+	. $prefage_directory/git/prompt.sh
+
+	# Change command prompt
+	export GIT_PS1_SHOWDIRTYSTATE=1
+	# '\u' adds the name of the current user to the prompt
+	# '\h' adds the name of the host
+	# '\W' adds the name of the current directory
+	# '\$(__git_ps1)' adds git-related stuff
+	export PS1="$lime\u$darkgraylight@$green\h $bluebright\W$green\$(__git_ps1) $lime$ $reset"
+fi
--- install-or-update.bash
+++ install-or-update.bash
@@ -1,67 +1,60 @@
 #!/bin/bash
 
+# move to working directory
+cd $( dirname "${BASH_SOURCE[0]}" )
+prefage_directory=$(pwd)
+
 # remove old prefage files
 ./uninstall.bash
 
-# common bash preferences
-echo "copying prefage bash scripts to home directory..."
-cp -iv .prefage-bash-include.bash ~/
-cp -iv .prefage-bash-ubuntu14.04.bash ~/
-cp -iv .prefage-bash-aliases.bash ~/
-cp -iv .prefage-bash-colors.bash ~/
-
-# include resources in .bashrc
+# create bashrc if it doesn't exist
 if [ ! -f ~/.bashrc ]; then
     echo "creating ~/.bashrc"
     touch ~/.bashrc
 fi
 
-if ! $(grep -q .prefage-bash-include.bash ~/.bashrc); then
-	echo "adding prefage references to ~/.bashrc"
-	echo "" >> ~/.bashrc
-	echo "# include prefage resources" >> ~/.bashrc
-	echo "source ~/.prefage-bash-include.bash" >> ~/.bashrc
+# include bash resources
+# check if references already in bashrc
+if $(grep -q prefage-bashrc-include.bash ~/.bashrc); then
+	echo -e "
+	WARNING: prefage-bashrc-include.bash already in ~/.bashrc
+	If you're having problems, check the line ordering for overrides
+	"
+# add references
 else
-	echo "WARNING: .prefage-bash-include.bash already in ~/.bashrc"
-	echo "   if you're having problems, check the line ordering for overrides"
+	echo "adding prefage references to ~/.bashrc"
+	echo -e "
+	# include prefage resources
+	source $prefage_directory/bash/prefage-bashrc-include.bash" >> ~/.bashrc
 fi
 
-
-# common git preferences
-# copy scripts to home directory
+# include git resources
 if $(command -v git >/dev/null 2>&1); then
-	echo "copying git scripts and configuration files to home directory..."
-	cp -iv .prefage-git-completion.bash ~/
-	cp -iv .prefage-git-config ~/
-	cp -iv .prefage-git-prompt.sh ~/
-	cp -iv .prefage-git-ignore ~/
-	# include resources in .gitconfig
+	# create gitconfig if it doesn't exist
 	if [ ! -f ~/.gitconfig ]; then
 	    echo "creating ~/.gitconfig"
 	    touch ~/.gitconfig
 	fi
-	if ! $(grep -q .prefage-git-config ~/.gitconfig); then
-		echo "including prefage-git-config to ~/.gitconfig"
-		echo "[include]" >> ~/.gitconfig
-		echo "	path = ~/.prefage-git-config" >> ~/.gitconfig
+	# check if references already included
+	if ! $(grep -q prefage-git-config-include ~/.gitconfig); then
+		echo "adding prefage-git-config-include to ~/.gitconfig"
+		echo -e "[include]
+		path = $prefage_directory/git/prefage-git-config-include" >> ~/.gitconfig
 	fi
 
 	# if meld installed, set git diff default external
 	if $(command -v meld >/dev/null 2>&1); then
 		echo "setting meld as git diff external tool"
-		cp -iv .prefage-git-meld.bash ~/
-		echo "[diff]" >> ~/.prefage-git-config
-		echo "	external = ~/.prefage-git-meld.bash" >> ~/.prefage-git-config
+		echo -e "[diff]
+		external = $prefage_directory/git/meld.bash" >> $prefage_directory/git/prefage-git-config-include
 	else
 		echo "If this is a desktop, you might consider installing meld."
 	fi
 fi
 
-
 # source .bashrc to activate changes
-echo
-echo "installation complete."
-echo "in order to activate new changes type:"
-echo
-echo "   source ~/.bashrc"
-echo
+echo -e "
+prefage installation complete.
+in order to activate new changes type:
+   source ~/.bashrc
+"
