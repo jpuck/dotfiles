62d40c80c8dcefa11f54d7137546edc2929d0876
administrator@Waltahr <jpuckett@walton.uark.edu>
2015-12-08 10:58:13 -0600

    add basic colors to View script

--- View-Colors.bash
+++ View-Colors.bash
@@ -1,10 +1,36 @@
 #!/bin/bash
+
+#
+# This file echoes a bunch of color codes to the terminal to demonstrate
+# what's available. Each line is the color code of one forground color,
+# out of 17 (default + 16 escapes), followed by a test use of that color
+# on all nine background colors (default + 8 escapes).
+#
+
+echo "basic colors:"
+
+T='gYw'   # The test text
+echo -e "\n                 40m     41m     42m     43m     44m     45m     46m     47m";
+for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' '  36m' '1;36m' '  37m' '1;37m';
+  do FG=${FGs// /}
+  echo -en " $FGs \033[$FG  $T  "
+  for BG in 40m 41m 42m 43m 44m 45m 46m 47m;
+    do echo -en "$EINS \033[$FG\033[$BG  $T \033[0m\033[$BG \033[0m";
+  done
+  echo;
+done
+echo
+
+
 #
 # generates an 8 bit color table (256 colors) for
 # reference purposes, using the \033[48;5;${val}m
 # ANSI CSI+SGR (see "ANSI Code" on Wikipedia)
 # http://bitmote.com/index.php?post/2012/11/19/Using-ANSI-Color-Codes-to-Colorize-Your-Bash-Prompt-on-Linux
 #
+
+echo "256 colors:"
+
 echo -en "\n   +  "
 for i in {0..35}; do
   printf "%2b " $i
