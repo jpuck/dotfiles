dfe83daaae647ebc20c5099b59400116270c22c8
administrator@Waltahr <jpuckett@walton.uark.edu>
2015-12-08 11:43:08 -0600

    fix missing git_ps1 sh

--- bash/prefage-bashrc-include.bash
+++ bash/prefage-bashrc-include.bash
@@ -16,14 +16,6 @@ if $(command -v git >/dev/null 2>&1); then
 	# '\W' adds the name of the current directory
 	# '\$(__git_ps1)' adds git-related stuff
 
-	if [[ $EUID -eq 0 ]]; then
-		if [[ $(__git_ps1) ]]; then
-			export PS1="$red\u$darkgraylight@$green\h $bluebright\W$green\$(__git_ps1) $lime$ $reset"
-		else
-			export PS1="$red\u$darkgraylight@$green\h $bluebright\W $lime$ $reset"
-		fi
-	else
-		export PS1="$lime\u$darkgraylight@$green\h $bluebright\W$green\$(__git_ps1) $lime$ $reset"
-	fi
+	export PS1="$(if [[ $EUID -eq 0 ]]; then echo $red; else echo $lime; fi)\u$darkgraylight@$green\h $bluebright\W\$( if true; then echo -e \"$green$(__git_ps1)\"; fi) $lime$ $reset"
 
 fi
