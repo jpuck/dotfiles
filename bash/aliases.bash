bf1644ad049c2d156ddbed86376a5171e7c7d90b
Jeff Puckett <jeff@jeffpuckett.com>
2016-03-15 10:32:41 -0500

    add custom ls time-style

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -1,4 +1,4 @@
-alias l='ls -la'
+alias l='ls -la --time-style="+%Y-%m-%d %a %T"'
 alias c='clear'
 alias crontaball="cat /etc/passwd | sed 's/^\([^:]*\):.*$/sudo crontab -u \1 -l 2>\&1/' | sh | grep -v 'no crontab for'"
 alias glg="git log --graph --oneline --decorate --all"
