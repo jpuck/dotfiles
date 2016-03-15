ede55b8b23da302d0cc8615eab06bea656e8ba04
Jeff Puckett <jeff@jeffpuckett.com>
2016-03-14 21:23:17 -0500

    add alias for git log graph

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -1,3 +1,4 @@
 alias l='ls -la'
 alias c='clear'
 alias crontaball="cat /etc/passwd | sed 's/^\([^:]*\):.*$/sudo crontab -u \1 -l 2>\&1/' | sh | grep -v 'no crontab for'"
+alias glg="git log --graph --oneline --decorate --all"
