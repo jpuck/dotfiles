393da24c7ac79b63edf8065c18f4ace0e833eea5
Jeff Puckett <jeff@jeffpuckett.com>
2016-04-18 16:45:19 -0500

    add composer installer

---
+++ apps.install/ubuntu/composer.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+# https://getcomposer.org/download/
+php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
+php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === '7228c001f88bee97506740ef0888240bd8a760b046ee16db8f4095c0d8d525f2367663f22a46b48d072c816e7fe19959') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+sudo mv composer.phar /usr/local/bin/composer
