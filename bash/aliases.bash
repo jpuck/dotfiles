42eb899f5f19ef65c4a16d0f362e0d51b2cbc75f
admonkey <jeffpuckett2@gmail.com>
2016-05-09 22:11:16 -0500

    add alias npm-exec

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -4,3 +4,5 @@ alias crontaball="cat /etc/passwd | sed 's/^\([^:]*\):.*$/sudo crontab -u \1 -l 
 alias glg="git log --graph --oneline --decorate --all"
 alias gpom="git push origin master"
 alias gup="~/prefage/git/upstream.clone.bash"
+# http://stackoverflow.com/questions/9679932/how-to-use-package-installed-locally-in-node-modules
+alias npm-exec='PATH=$(npm bin):$PATH'
