c5a0fcc48ed06f8244fe742d46f57e2390f30cd8
Jeff Puckett <jeff@jeffpuckett.com>
2016-05-23 21:58:52 -0500

    use gup just for template project

--- git/upstream.clone.bash
+++ git/upstream.clone.bash
@@ -13,16 +13,16 @@ if [ $# -eq 0 ]; then
 fi
 
 # default upstream repo
-if [ -z ${2+x} ]; then
+#if [ -z ${2+x} ]; then
   REPOSITORY="https://github.com/jeff-puckett/WebSite.git"
-else
-  REPOSITORY=$2
-fi
+#else
+#  REPOSITORY=$2
+#fi
 
 # repo aliases
-if [ "$2" = "html5" ] ; then
-  REPOSITORY="https://github.com/admonkey/html5-boilerplate.git"
-fi
+#if [ "$2" = "html5" ] ; then
+#  REPOSITORY="https://github.com/admonkey/html5-boilerplate.git"
+#fi
 
 # clone repository, name remote "upstream", name new repo $1
 git clone -o upstream $REPOSITORY $1
@@ -30,15 +30,16 @@ git clone -o upstream $REPOSITORY $1
 # move into repo
 cd $1
 
-# document upstream remote
-echo "$(date "+%Y-%m-%d %a %T") $REPOSITORY" >> upstream.remote.git.txt
-git add upstream.remote.git.txt
-
 # check if master branch already exists
 #git rev-parse --verify master
 
 # create new master branch
-#git checkout -b master
+git checkout -b master
+
+# document upstream remote
+echo "$(date "+%Y-%m-%d %a %T") $REPOSITORY" >> upstream.remote.git.txt
+git add upstream.remote.git.txt
+git commit -m "add upstream.remote.git.txt"
 
 # create new remote repository on github
 github_create
