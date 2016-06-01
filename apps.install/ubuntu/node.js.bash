df133bfd0a62f4d29a26a180b3b932bfd7a6403b
admonkey <jeffpuckett2@gmail.com>
2016-06-01 14:13:51 -0500

    install nvm

--- apps.install/ubuntu/node.js.bash
+++ apps.install/ubuntu/node.js.bash
@@ -1,6 +1,18 @@
 #!/bin/bash
-curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
-sudo apt-get install -y nodejs
+
+# install nvm
+# manually check for updates to install script
+# https://github.com/creationix/nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
+# restart shell
+nvm install stable
+nvm alias default stable
+
+#
+# old instructions to install via sudo
+#
+# curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+# sudo apt-get install -y nodejs
 
 # upgrade npm
-sudo npm update npm -g
+# sudo npm update npm -g
