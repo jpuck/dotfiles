bc629f82bff962e273d9429afb1394f3e6912934
Jeff Puckett <jeff@jeffpuckett.com>
2016-07-23 23:05:52 -0500

    add rvm installer for ubuntu

---
+++ apps.install/ubuntu/ruby.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# http://stackoverflow.com/a/34523631/4233593
+
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+
+\curl -sSL https://get.rvm.io | bash # install RVM (development version)
+
+rvm get head # Make Sure RVM up to date
+
+rvm install ruby
