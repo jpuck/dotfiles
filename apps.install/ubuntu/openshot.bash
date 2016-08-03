047fcd62627d69e9b3db39862e81d1bf8d94213f
Jeff Puckett <jeff@jeffpuckett.com>
2016-08-03 10:48:09 -0500

    install ubuntu openshot

---
+++ apps.install/ubuntu/openshot.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# https://launchpad.net/~openshot.developers/+archive/ubuntu/ppa
+
+sudo add-apt-repository ppa:openshot.developers/ppa
+sudo apt-get update
+sudo apt-get install openshot-qt
