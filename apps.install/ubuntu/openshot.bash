688a3c7b51a172c84119f1051fd596c24581efad
admonkey <jeffpuckett2@gmail.com>
2016-08-28 11:54:35 -0500

    Delete openshot.bash

--- apps.install/ubuntu/openshot.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# https://launchpad.net/~openshot.developers/+archive/ubuntu/ppa
-
-sudo add-apt-repository ppa:openshot.developers/ppa
-sudo apt-get update
-sudo apt-get install openshot-qt
