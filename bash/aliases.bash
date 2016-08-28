0ce8b58c71b30d313f6503556f61878d5d8297e2
Jeff Puckett <jeff@jeffpuckett.com>
2016-08-28 12:13:04 -0500

    shorten tree -> tre alias

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -4,6 +4,6 @@ alias gup="~/prefage/git/upstream.clone.bash"
 # http://stackoverflow.com/a/15157360/4233593#how-to-use-package-installed-locally-in-node-modules
 alias nexec='PATH=$(npm bin):$PATH'
 alias asr='sudo service apache2 restart'
-alias tree="tree -I 'vendor|node_modules'"
+alias tre="tree -I 'vendor|node_modules'"
 alias o='xdg-open'
 alias brw='byzanz-record-window'
