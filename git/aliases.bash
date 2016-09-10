4638728e9a0d8d8fcc098fe3d2852faf94dae7af
Jeff Puckett <jeff@jeffpuckett.com>
2016-09-10 16:00:46 -0500

    fix delete remote branches if not HEAD or empty

--- git/aliases.bash
+++ git/aliases.bash
@@ -44,7 +44,7 @@ alias gup="$prefage_directory/git/upstream.clone.bash"
 git_clean_merged_remote(){
 	if [ -z $1 ]; then echo "error: remote required"; return; fi
 	if [ -z $2 ]; then echo "error: branch required"; return; fi
-	git branch -a --merged remotes/$1/$2 | grep -v $2 | grep "remotes/$1/" | cut -d "/" -f 3 | xargs -n 1 git push --delete $1
+	git branch -a --merged remotes/$1/$2 | grep -v "HEAD\|master\|dev\|develop\|$2" | grep "remotes/$1/" | cut -d "/" -f 3 | xargs -r git push --delete $1
 }
 
 alias gcr='git_clean_merged_remote'
