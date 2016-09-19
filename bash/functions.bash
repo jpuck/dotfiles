8589666fa1f9c21bf0530f19be4867d58a30044f
Jeff Puckett <jeff@jeffpuckett.com>
2016-09-18 21:43:00 -0500

    upgrade password generator complexity

--- bash/functions.bash
+++ bash/functions.bash
@@ -2,6 +2,6 @@
 # http://www.cyberciti.biz/faq/linux-random-password-generator/
 genpasswd() {
 	local l=$1
-       	[ "$l" == "" ] && l=16
-      	tr -dc A-Za-z0-9_ < /dev/urandom | head -c ${l} | xargs
+	[ "$l" == "" ] && l=16
+	tr -dc \\050-\\172 < /dev/urandom | head -c ${l} | xargs
 }
