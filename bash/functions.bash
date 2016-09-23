f753458fe6953cca8990e754ff83ebaefdfac14d
Jeff Puckett <jeff@jeffpuckett.com>
2016-09-23 13:11:35 -0500

    doc security issue with genpasswd

--- bash/functions.bash
+++ bash/functions.bash
@@ -1,5 +1,8 @@
-# thanks vivek@nixCraft
 # http://www.cyberciti.biz/faq/linux-random-password-generator/
+# WARNING: On a multiuser Linux system
+# everyone can see your password
+# by looking at the process list.
+# https://tech.slashdot.org/comments.pl?sid=2679771&cid=39091343
 genpasswd() {
 	local l=$1
 	[ "$l" == "" ] && l=16
