b01607aa01edbd64d26aa2cf9b801f3fc53016f6
Jeff Puckett <jeff@jeffpuckett.com>
2016-09-29 15:48:27 -0500

    Update lamp.bash

--- apps.install/ubuntu/lamp.bash
+++ apps.install/ubuntu/lamp.bash
@@ -5,5 +5,5 @@
 sudo apt install apache2 -y
 sudo apt install mysql-server php7.0-mysql -y
 mysql_secure_installation
-sudo apt install php libapache2-mod-php php7.0-zip php7.0-mbstring php-xml -y
+sudo apt install php libapache2-mod-php php7.0-zip php7.0-mbstring php-xml php-curl -y
 sudo service apache2 restart
