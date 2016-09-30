8b1a3bead9a01a9f1e8534015c7fdafb59f95517
Jeff Puckett <jeff@jeffpuckett.com>
2016-09-29 22:10:34 -0500

    doc aliases

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -7,7 +7,11 @@ alias lt='l -tr'
 alias lf='l -p --color=always | grep -v /'
 # list files sort by time
 alias lft='l -ptr --color=always | grep -v /'
+
+# show every users crontab
 alias crontaball="cat /etc/passwd | sed 's/^\([^:]*\):.*$/sudo crontab -u \1 -l 2>\&1/' | sh | grep -v 'no crontab for'"
+
+# npm execute
 # http://stackoverflow.com/a/15157360/4233593#how-to-use-package-installed-locally-in-node-modules
 alias nexec='PATH=$(npm bin):$PATH'
 alias asr='sudo service apache2 restart'
@@ -19,13 +23,22 @@ alias t='phpunit'
 alias tt='phpunit --testdox'
 alias k='kate'
 alias less='less --tabs=4'
+
+# make a file and any folders along the way
 alias mk='install -m=664 -b -D /dev/null'
+# make an executable and any folders along the way
 alias mkx='install -b -D /dev/null'
+# make a folder and any folders along the way
 alias mkd='mkdir -p'
+# remove directory
 alias rd='rm -rf'
 alias myr='mysql -u root -p'
+
+# delete trailing spaces from a file
 # http://stackoverflow.com/a/20521936/4233593
 alias deltrailspaces="sed -i 's/\s*$//'"
+
+# view coverage
 alias vc='php -S localhost:8080 -t coverage/'
 
 # save change directory
