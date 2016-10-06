ae07919771698aa8883e2996b3e1df38f69ea79b
Jeff Puckett <jeff@jeffpuckett.com>
2016-10-05 21:33:11 -0500

    update node version mgr install script

--- apps.install/ubuntu/node.js.bash
+++ apps.install/ubuntu/node.js.bash
@@ -3,16 +3,8 @@
 # install nvm
 # manually check for updates to install script
 # https://github.com/creationix/nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
-# restart shell
-nvm install stable
-nvm alias default stable
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
 
-#
-# old instructions to install via sudo
-#
-# curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
-# sudo apt-get install -y nodejs
-
-# upgrade npm
-# sudo npm update npm -g
+# restart terminal, then:
+# nvm install stable
+# nvm alias default stable
