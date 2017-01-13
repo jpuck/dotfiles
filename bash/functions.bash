7291480969e7fb495558f003c25a3a96bb0ca223
Jeff Puckett <jeff@jeffpuckett.com>
2017-01-13 21:43:43 +0000

    add genpasswdc

--- bash/functions.bash
+++ bash/functions.bash
@@ -4,11 +4,28 @@
 # by looking at the process list.
 # https://tech.slashdot.org/comments.pl?sid=2679771&cid=39091343
 genpasswd() {
-	local length="$1"
-	[ "$length" == "" ] && length=16
-	# dash is escaped \-
-	tr -dc [:alpha:][:digit:]'{[(</|>)]}~!@#^&\-_=+;:,.?' < /dev/urandom | head -c "$length"
-	echo
+    # option -n (do not echo newline after password)
+    # optional argument 1 (length of password)
+    local OPTIND option newline=1
+    while getopts "n" option; do
+        case "$option" in
+            n)
+                newline=0
+                ;;
+        esac
+    done
+    shift $((OPTIND-1))
+
+    local length="$1"
+    [ "$length" == "" ] && length=16
+    # dash is escaped \-
+    tr -dc [:alpha:][:digit:]'{[(</|>)]}~!@#^&\-_=+;:,.?' < /dev/urandom | head -c "$length"
+
+    (($newline)) && echo
+}
+
+genpasswdc() {
+    genpasswd -n "$1" | xclip -selection clipboard
 }
 
 # NOTE: does not count newlines
