0c7918f9df596ad896d9033cbd7ed105f4233509
Jeff Puckett <jeff@jeffpuckett.com>
2017-02-22 04:17:33 +0000

    alias m artisan migrate

--- php/aliases.bash
+++ php/aliases.bash
@@ -4,6 +4,7 @@ alias artt='art tinker'
 alias vw='tre resources/views'
 alias rl='art route:list'
 alias ms='art migrate:status'
+alias m='art migrate'
 
 # testing
 alias phpunit='./vendor/bin/phpunit'
