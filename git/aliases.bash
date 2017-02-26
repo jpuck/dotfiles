8c2c44d1992459e206255c25a9582b0c7eaf6423
Jeff Puckett <jeff@jeffpuckett.com>
2017-02-26 02:22:45 +0000

    add gca function git clean merged all

--- git/aliases.bash
+++ git/aliases.bash
@@ -63,7 +63,6 @@ git_clean_merged_remote(){
 	if [ -z $2 ]; then echo "error: branch required"; return; fi
 	git branch -a --merged remotes/$1/$2 | grep -v "HEAD\|master\|dev\|develop\|$2" | grep "remotes/$1/" | cut -d "/" -f 3 | xargs -r git push --delete $1
 }
-
 alias gcr='git_clean_merged_remote'
 __git_complete gcr _git_pull
 
@@ -71,6 +70,12 @@ git_clean_merged_local(){
 	if [ -z $1 ]; then echo "error: branch required"; return; fi
 	git branch --merged | egrep -v "(^\*|master|dev|develop|$1)" | xargs git branch -d
 }
-
 alias gcl='git_clean_merged_local'
 __git_complete gcl _git_checkout
+
+git_clean_merged_all(){
+    gcr "$1" "$2"
+    gcl "$2"
+}
+alias gca="git_clean_merged_all"
+__git_complete gca _git_pull
