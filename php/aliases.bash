4b14d82fdf5018f157f6be940b554dd8e0d994da
Jeff Puckett <jeff@jeffpuckett.com>
2017-06-05 18:44:45 +0000

    stop using docker for all tests

--- php/aliases.bash
+++ php/aliases.bash
@@ -7,8 +7,8 @@ alias ms='art migrate:status'
 alias m='art migrate'
 
 # testing
-alias t='docker run --rm -it -v "$PWD":/code gpgl/test-core'
-alias tt='t ./vendor/bin/phpunit --testdox'
-alias ttt='t ./vendor/bin/phpunit --debug'
+alias t='./vendor/bin/phpunit'
+alias tt='t --testdox'
+alias ttt='t --debug'
 alias phpspec='vendor/bin/phpspec'
 alias dusk='art dusk --stop-on-failure'
