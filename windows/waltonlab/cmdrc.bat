a120d7b8de57f7f7f2bd0694a62a23d4a114d4dc
Jeff Puckett <jeff@jeffpuckett.com>
2017-10-27 16:01:45 +0000

    add windows rc

---
+++ windows/waltonlab/cmdrc.bat
@@ -0,0 +1,33 @@
+:: https://superuser.com/a/144348/571125
+@echo off
+
+:: files
+doskey l=dir
+doskey rm=del $*
+
+:: git
+git config --global user.name "Jeff Puckett"
+git config --global user.email "jeff@jeffpuckett.com"
+doskey la=git log --graph --oneline --decorate --all
+doskey s=git status
+doskey b=git branch -avv
+doskey co=git checkout $*
+doskey d=git diff $*
+doskey dc=git diff --cached $*
+doskey a=git add $*
+doskey au=git add -u $*
+doskey r=git reset $*
+doskey cm=git commit $*
+doskey cmm=git commit -m $*
+doskey cam=git commit -am $*
+doskey ammend=git commit --amend --no-edit
+doskey show=git show $*
+doskey showl=git show --stat --summary $*
+doskey gr=git remote -v
+doskey ph=git push $*
+doskey pl=git pull $*
+
+:: bin
+doskey vim="C:\Program Files\Git\usr\bin\vim" $*
+doskey curl="C:\Program Files\Git\mingw64\bin\curl" $*
+set PATH=%PATH%;S:\jpucket\bin
---
+++ windows/waltonlab/reg.bat
@@ -0,0 +1,4 @@
+REG ADD "HKEY_LOCAL_MACHINE\Software\Microsoft\Command Processor" /v AutoRun /d "S:\jpucket\dotfiles\windows\waltonlab\cmdrc.bat"
+pause
+cls
+exit
