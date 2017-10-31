edc975739c619a90b1492a0f3f5438897ac5bbdd
Jeff Puckett <jeff@jeffpuckett.com>
2017-10-31 02:40:17 +0000

    add show file extensions to regedit

--- windows/waltonlab/reg.bat
+++ windows/waltonlab/reg.bat
@@ -1,4 +1,9 @@
 REG ADD "HKEY_LOCAL_MACHINE\Software\Microsoft\Command Processor" /v AutoRun /d "S:\jpucket\dotfiles\windows\waltonlab\cmdrc.bat"
+
+:: add show file extensions to regedit
+:: https://superuser.com/a/666895/571125
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0 /f
+
 pause
 cls
 exit
