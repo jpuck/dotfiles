0809d6519fca4299bafed597ad13ab14f2542be9
Jeff Puckett <jeff@jeffpuckett.com>
2017-10-31 02:43:44 +0000

    doc load rc every command prompt

--- windows/waltonlab/reg.bat
+++ windows/waltonlab/reg.bat
@@ -1,3 +1,4 @@
+:: load this rc file every time a command prompt is opened
 REG ADD "HKEY_LOCAL_MACHINE\Software\Microsoft\Command Processor" /v AutoRun /d "S:\jpucket\dotfiles\windows\waltonlab\cmdrc.bat"
 
 :: add show file extensions to regedit
