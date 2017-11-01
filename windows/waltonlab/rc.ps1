2d0c43fdac9a19f2c886dafc75ecb881453d457c
Jeff Puckett <jeff@jeffpuckett.com>
2017-11-01 16:22:37 +0000

    add pin to favorites

---
+++ windows/waltonlab/rc.ps1
@@ -0,0 +1,2 @@
+$o = New-Object -com shell.application
+$o.Namespace('S:\jpucket').Self.InvokeVerb("pintohome")
--- windows/waltonlab/reg.bat
+++ windows/waltonlab/reg.bat
@@ -5,6 +5,10 @@ REG ADD "HKEY_LOCAL_MACHINE\Software\Microsoft\Command Processor" /v AutoRun /d 
 :: https://superuser.com/a/666895/571125
 REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0 /f
 
+:: pin favorite folders
+net use S: \\Ent-Storage1.walton.uark.edu\Enterprise$
+Powershell.exe -executionpolicy Unrestricted -File  S:\jpucket\dotfiles\windows\waltonlab\rc.ps1
+
 pause
 cls
 exit
