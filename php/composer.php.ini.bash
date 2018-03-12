68bfca3c0da59cecb2a8a00ff685697fec91ab7e
Jeff Puckett <jeff@jeffpuckett.com>
2018-03-11 21:17:35 -0500

    fix composer missing ini file on mac...

--- php/composer.php.ini.bash
+++ php/composer.php.ini.bash
@@ -8,11 +8,7 @@
 # "Scan for additional .ini files in:" directory.
 
 if $(command -v composer >/dev/null 2>&1); then
-	which_composer="$(which composer)";
-	composer_php_ini="$prefage_directory/php/composer.php.ini";
-	current_php_ini="$(php --ini | grep 'Loaded Configuration File:' | awk '{print $4}')";
-	cat "$current_php_ini" | grep -v xdebug > "$composer_php_ini";
-	alias c="php -c '$composer_php_ini' '$which_composer'";
+	alias c='composer';
 	alias cda="c dumpautoload"
 	export PATH="$HOME/.config/composer/vendor/bin:$HOME/.composer/vendor/bin:$PATH";
 fi
