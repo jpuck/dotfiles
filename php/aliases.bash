9bf8cf554f3321f4c9957b619c428081ef7c9de7
Jeff Puckett <jeff@jeffpuckett.com>
2019-06-19 21:01:01 +0000

    remove old aliases

--- php/aliases.bash
+++ php/aliases.bash
@@ -1,15 +1,11 @@
 # laravel
 alias art='php artisan'
-alias sv='php artisan serve'
 alias artt='art tinker'
-alias vw='tre resources/views'
 alias rl='art route:list'
 alias ms='art migrate:status'
-alias m='art migrate'
 
 # testing
 alias t='./vendor/bin/phpunit'
 alias tt='t --testdox'
 alias ttt='t --debug'
-alias phpspec='vendor/bin/phpspec'
 alias dusk='art dusk --stop-on-failure'
