e35eb4586c095182a73a8ffb2450a171d61e5d97
Jeff Puckett <jeff@jeffpuckett.com>
2020-02-14 05:50:35 +0000

    fix basic color code chart

--- View-Colors.bash
+++ View-Colors.bash
@@ -9,12 +9,11 @@
 
 echo "basic colors:"
 
-for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' '  36m' '1;36m' '  37m' '1;37m';
-  do FG=${FGs// /}
-  for BG in 40m 41m 42m 43m 44m 45m 46m 47m;
-    do echo -en "$EINS \033[$FG\033[$BG  $FGs$BG \033[0m\033[$BG \033[0m";
+for FG in 30 31 32 33 34 35 36 37; do
+  for BG in 40 41 42 43 44 45 46 47; do
+    echo -en " \033[0;$FG;${BG}m 0;$FG;${BG}m \033[1;$FG;${BG}m 1;$FG;${BG}m \033[0m";
   done
-  echo;
+  echo
 done
 echo
 
