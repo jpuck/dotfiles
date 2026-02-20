6489f74f0a21a4dcc81d5d088f61534aa3cf87e7
Jeff Puckett <jeff@jeffpuckett.com>
2026-02-20 02:51:37 +0000

    add node js

--- apps.install/ubuntu/node.js.bash
+++ apps.install/ubuntu/node.js.bash
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+set -e
+
 # install nvm
 # manually check for updates to install script
 # https://github.com/creationix/nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
 
-# restart terminal, then:
-# nvm install stable
-# nvm alias default stable
+# reload env for nvm paths and completion in prefage-include
+. ~/.bashrc
+
+# LTS
+nvm install --lts
